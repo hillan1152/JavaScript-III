226a6d319515b09252b5bf596f38549342996915
Anthony Hill <hillan1152@gmail.com>
2019-08-21 17:00:28 -0400

    Going insane

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(createdAt, name, dimensions) {
+  this.createdAt = createdAt,
+  this.name = name,
+  this.dimensions = dimensions
+}
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`
+}
 
 /*
   === CharacterStats ===
@@ -22,6 +30,17 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(healthpoints) {
+  this.healthpoints = healthpoints;
+  this.createdAt = createdAt,
+  this.name = name,
+  this.dimensions = dimensions
+  GameObject.call(this);
+}
+CharacterStats.prototype.takeDamage = function(name) {
+  return `${this.name} took damage.`
+}
+console.log(CharacterStats());
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +51,22 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(team, weapons, language, healthpoints, createdAt, name, dimensions) {
+  this.team = team;
+  this.weapons = weapons;
+  this.language = language;
+  this.healthpoints = healthpoints;
+  this.createdAt = createdAt,
+  this.name = name,
+  this.dimensions = dimensions
+  GameObject.call(this);
+  CharacterStats.call(this);
+}
+
+Humanoid.prototype.greet = function() {
+  `${this.name} offers a  greeting in ${this.language}.`
+}
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +76,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
